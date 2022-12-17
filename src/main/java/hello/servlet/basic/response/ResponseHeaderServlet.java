7ac82b7518421bb6684438c69c23caa31f581825
이충재 <knm8643@nate.com>
2022-12-18 00:51:33 +0900

    [서블릿] HttpServletResponse - 기본사용법 / 22-12-18

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,6 +26,8 @@ public class ResponseHeaderServlet extends HttpServlet {
 
         //[Header 편의 메서드]
         content(response);
+        cookie(response);
+        redirect(response);
 
         PrintWriter writer = response.getWriter();
         writer.println("ok");
@@ -37,5 +40,22 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
         // response.setContentLength(2); // (생략시 자동 생성)
+    }
+
+    private void cookie(HttpServletResponse response){
+        //Set-Cookie : myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Statup Code 302
+        //Location : /basic/hello-form.html
+
+        //response.setStatus(HttpServetResponse.SC_FOUND); // 302
+        //response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
     }
 }
